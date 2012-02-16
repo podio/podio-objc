044a799814879151b5d504faf83627f143dc7c41
Sebastian Rehnby <sebastian.rehnby@gmail.com>
2012-02-16 10:20:35 +0100

    Added create/update item to PKItemAPI

--- PodioKit/Model/PKItemAPI.h
+++ PodioKit/Model/PKItemAPI.h
@@ -12,5 +12,8 @@
 @interface PKItemAPI : PKBaseAPI
 
 + (PKRequest *)requestForItemWithId:(NSUInteger)itemId;
++ (PKRequest *)requestToCreateItemWithAppId:(NSUInteger)appId fields:(NSArray *)fields fileIds:(NSArray *)fileIds;
++ (PKRequest *)requestToUpdateItemFields:(NSArray *)fields itemId:(NSUInteger)itemId;
++ (PKRequest *)requestToUpdateItemWithId:(NSUInteger)itemId fields:(NSArray *)fields fileIds:(NSArray *)fileIds;
 
 @end
--- PodioKit/Model/PKItemAPI.m
+++ PodioKit/Model/PKItemAPI.m
@@ -14,4 +14,34 @@
   return [PKRequest requestWithURI:[NSString stringWithFormat:@"/item/%d", itemId] method:PKAPIRequestMethodGET];
 }
 
++ (PKRequest *)requestToCreateItemWithAppId:(NSUInteger)appId fields:(NSArray *)fields fileIds:(NSArray *)fileIds {
+  PKRequest *request = [PKRequest requestWithURI:[NSString stringWithFormat:@"/item/app/%d/", appId] method:PKAPIRequestMethodPOST];
+  
+  request.body = [NSMutableDictionary dictionaryWithObject:fields forKey:@"fields"];
+  if (fileIds != nil && [fileIds count] > 0) {
+    [request.body setObject:fileIds forKey:@"file_ids"];
+  }
+  
+  return request;
+}
+
++ (PKRequest *)requestToUpdateItemFields:(NSArray *)fields itemId:(NSUInteger)itemId {
+  PKRequest *request = [PKRequest requestWithURI:[NSString stringWithFormat:@"/item/%d/value", itemId] method:PKAPIRequestMethodPUT];
+  
+  request.body = [NSMutableDictionary dictionaryWithObject:fields forKey:@"fields"];
+  
+  return request;
+}
+
++ (PKRequest *)requestToUpdateItemWithId:(NSUInteger)itemId fields:(NSArray *)fields fileIds:(NSArray *)fileIds {
+  PKRequest *request = [PKRequest requestWithURI:[NSString stringWithFormat:@"/item/%d", itemId] method:PKAPIRequestMethodPUT];
+  
+  request.body = [NSMutableDictionary dictionaryWithObject:fields forKey:@"fields"];
+  if (fileIds != nil && [fileIds count] > 0) {
+    [request.body setObject:fileIds forKey:@"file_ids"];
+  }
+  
+  return request;
+}
+
 @end
