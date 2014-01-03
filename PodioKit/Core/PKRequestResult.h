46ec217b68d27218f41df9c5d97277cef9383498
Sebastian Rehnby <sebastian@rehnby.com>
2014-01-03 09:22:57 +0100

    Added missing property qualifiers to PKRequestResult

--- PodioKit/Core/PKRequestResult.h
+++ PodioKit/Core/PKRequestResult.h
@@ -11,11 +11,11 @@
 @interface PKRequestResult : NSObject
 
 @property (readonly) NSUInteger responseStatusCode;
-@property (readonly) NSString *responseString;
-@property (readonly) id responseData; // The raw data
-@property (readonly) id parsedData; // The returned data parsed into native containers
-@property (readonly) id objectData; // The parsed object(s) data. Same as parsedData unless the objectDataPathComponents property of PKRequestOperation was used
-@property (readonly) id resultData; // The final mapped object(s)
+@property (readonly, copy) NSString *responseString;
+@property (readonly, strong) id responseData; // The raw data
+@property (readonly, strong) id parsedData; // The returned data parsed into native containers
+@property (readonly, strong) id objectData; // The parsed object(s) data. Same as parsedData unless the objectDataPathComponents property of PKRequestOperation was used
+@property (readonly, strong) id resultData; // The final mapped object(s)
 @property (readonly) NSInteger responseObjectCount;
 
 + (PKRequestResult *)resultWithResponseStatusCode:(NSUInteger)responseStatusCode responseData:(id)responseData parsedData:(id)parsedData objectData:(id)objectData resultData:(id)resultData;
