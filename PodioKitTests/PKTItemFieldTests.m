b61cb6ecd2afd0b9eaadcb4e6a299b8f232ff88e
Sebastian Rehnby <sebastian@rehnby.com>
2014-04-28 21:47:14 +0200

    Basic tests for setting field values

--- PodioKitTests/PKTItemFieldTests.m
+++ PodioKitTests/PKTItemFieldTests.m
@@ -17,16 +17,53 @@
 
 @implementation PKTItemFieldTests
 
-- (void)testSetValue {
+- (void)testSetValues {
+  PKTItemField *field = [[PKTItemField alloc] init];
+  [field setValues:@[@"First value", @"Second value"]];
   
+  expect(field.values).to.haveCountOf(2);
+  expect([field valueAtIndex:0]).to.equal(@"First value");
+  expect([field valueAtIndex:1]).to.equal(@"Second value");
+}
+
+- (void)testSetFirstValue {
+  PKTItemField *field = [[PKTItemField alloc] init];
+  [field setFirstValue:@"Some text"];
+  
+  expect(field.values).to.haveCountOf(1);
+  expect([field firstValue]).to.equal(@"Some text");
 }
 
 - (void)testAddValue {
+  PKTItemField *field = [[PKTItemField alloc] init];
+  [field addValue:@"First value"];
+  [field addValue:@"Second value"];
   
+  expect(field.values).to.haveCountOf(2);
+  expect([field valueAtIndex:0]).to.equal(@"First value");
+  expect([field valueAtIndex:1]).to.equal(@"Second value");
 }
 
 - (void)testRemoveValue {
+  PKTItemField *field = [[PKTItemField alloc] init];
+  [field addValue:@"First value"];
+  [field addValue:@"Second value"];
   
+  [field removeValue:@"First value"];
+  
+  expect(field.values).to.haveCountOf(1);
+  expect([field valueAtIndex:0]).to.equal(@"Second value");
+}
+
+- (void)testRemoveValueAtIndex {
+  PKTItemField *field = [[PKTItemField alloc] init];
+  [field addValue:@"First value"];
+  [field addValue:@"Second value"];
+  
+  [field removeValueAtIndex:0];
+  
+  expect(field.values).to.haveCountOf(1);
+  expect([field valueAtIndex:0]).to.equal(@"Second value");
 }
 
 @end
