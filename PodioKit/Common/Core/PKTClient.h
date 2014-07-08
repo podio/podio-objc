de8e1a84f70671c5f6a355e2a0d5f9b087392d37
Sebastian Rehnby <sebastian@rehnby.com>
2014-07-08 14:15:32 +0200

    Additional documentation of PKTClient

--- PodioKit/Common/Core/PKTClient.h
+++ PodioKit/Common/Core/PKTClient.h
@@ -23,21 +23,103 @@ extern NSString * const PKTClientAuthenticationStateDidChangeNotification;
 @property (nonatomic, assign, readonly) BOOL isAuthenticated;
 @property (nonatomic, strong) id<PKTTokenStore> tokenStore;
 
+/**
+ *  The default API client.
+ *
+ *  @return The default client.
+ */
 + (instancetype)defaultClient;
+
+/**
+ *  The current API client. The current client for a give scope can be changed by 
+ *  using the performBlock: method.
+ *
+ *  @see performBlock:
+ *
+ *  @return The current client.
+ */
 + (instancetype)currentClient;
 
+/**
+ *  The designated initializer. Calling the default init method will instantiate a new HTTP client
+ *  instance and pass it to this method.
+ *
+ *  @param client The HTTP client for the client to use for creating HTTP requests.
+ *
+ *  @return The initialized client.
+ */
 - (instancetype)initWithHTTPClient:(PKTHTTPClient *)client;
 
+/** Configure the default client with a Podio API key/secret pair.
+ *
+ * @see https://developers.podio.com/api-key
+ *
+ * @param key The Podio API key
+ * @param secret The Podio API secret matching the key
+ */
 - (void)setupWithAPIKey:(NSString *)key secret:(NSString *)secret;
 
+/**
+ *  Execute a block for which the current client is self. This is useful to force the use
+ *  of a client instead of the default client for a certain scope, and enabled multiple clients to 
+ *  work in parallel.
+ *
+ *  @see currentClient
+ *
+ *  @param block The block for which the current client should be self.
+ */
 - (void)performBlock:(void (^)(void))block;
 
+/** Authenticate the default client as a user with an email and password.
+ *
+ *  @see https://developers.podio.com/authentication/username_password
+ *
+ *  @param email The user's email address
+ *  @param password The user's password
+ *  @param completion The completion block to be called once the authentication attempt completes either successfully or with an error.
+ *
+ *  @return The resulting request operation.
+ */
 - (AFHTTPRequestOperation *)authenticateAsUserWithEmail:(NSString *)email password:(NSString *)password completion:(PKTRequestCompletionBlock)completion;
+
+/** Authenticate the default client as an app.
+ *
+ *  @see https://developers.podio.com/authentication/app_auth
+ *
+ *  @param appID The ID of the application to authenticate as.
+ *  @param appToken The app token string associated with the app.
+ *  @param completion The completion block to be called once the authentication attempt completes either successfully or with an error.
+ *
+ *  @return The resulting request operation.
+ */
 - (AFHTTPRequestOperation *)authenticateAsAppWithID:(NSUInteger)appID token:(NSString *)appToken completion:(PKTRequestCompletionBlock)completion;
+
+/** Configure authentication parameters for authenticating the default client as an app.
+ *
+ * Instead of authenticating immediately, this method configures the default client to use the
+ * app ID and token to authenticate once whenever a request is performed without the client being
+ * authenticated.
+ *
+ * @param appID The id of the application to authenticate as.
+ * @param appToken The app token string associated with the app.
+ */
 - (void)authenticateAutomaticallyAsAppWithID:(NSUInteger)appID token:(NSString *)appToken;
 
+/**
+ *  Dispatches an HTTP request operation for the provided request.
+ *
+ *  @param request    The request to perform.
+ *  @param completion The completion handler to be called once the HTTP request operation completes.
+ *
+ *  @return The HTTP request operation.
+ */
 - (AFHTTPRequestOperation *)performRequest:(PKTRequest *)request completion:(PKTRequestCompletionBlock)completion;
 
+/**
+ *  Will attempt to restore the OAuth token from the current tokenStore if one has been configured.
+ *
+ *  @see tokenStore
+ */
 - (void)restoreTokenIfNeeded;
 
 @end
