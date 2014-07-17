256f8202ebec4db13f140ff6661057094886c983
Romain Briche <romain.briche@gmail.com>
2014-07-17 15:18:46 +0200

    Add Subscriptions API requests

---
+++ PodioKit/API/PKSubscriptionAPI.h
@@ -0,0 +1,17 @@
+//
+//  PKSubscriptionAPI.h
+//  PodioKit
+//
+//  Created by Romain Briche on 17/07/14.
+//  Copyright (c) 2014 Citrix Systems, Inc. All rights reserved.
+//
+
+#import "PKBaseAPI.h"
+
+@interface PKSubscriptionAPI : PKBaseAPI
+
++ (PKRequest *)requestToSubscribeWithReferenceId:(NSUInteger)referenceId referenceType:(PKReferenceType)referenceType;
++ (PKRequest *)requestToUnsubscribeWithReferenceId:(NSUInteger)referenceId referenceType:(PKReferenceType)referenceType;
++ (PKRequest *)requestToGetSubscriptionsWithReferenceId:(NSUInteger)referenceId referenceType:(PKReferenceType)referenceType;
+
+@end
---
+++ PodioKit/API/PKSubscriptionAPI.m
@@ -0,0 +1,34 @@
+//
+//  PKSubscriptionAPI.m
+//  PodioKit
+//
+//  Created by Romain Briche on 17/07/14.
+//  Copyright (c) 2014 Citrix Systems, Inc. All rights reserved.
+//
+
+#import "PKSubscriptionAPI.h"
+
+@implementation PKSubscriptionAPI
+
++ (PKRequest *)requestToSubscribeWithReferenceId:(NSUInteger)referenceId referenceType:(PKReferenceType)referenceType {
+	NSString * uri = [NSString stringWithFormat:@"/subscription/%@/%ld", [PKConstants stringForReferenceType:referenceType], (unsigned long)referenceId];
+  PKRequest *request = [PKRequest requestWithURI:uri method:PKRequestMethodPOST objectMapping:nil];
+
+  return request;
+}
+
++ (PKRequest *)requestToUnsubscribeWithReferenceId:(NSUInteger)referenceId referenceType:(PKReferenceType)referenceType {
+	NSString *uri = [NSString stringWithFormat:@"/subscription/%@/%ld", [PKConstants stringForReferenceType:referenceType], (unsigned long)referenceId];
+  PKRequest *request = [PKRequest requestWithURI:uri method:PKRequestMethodDELETE objectMapping:nil];
+
+  return request;
+}
+
++ (PKRequest *)requestToGetSubscriptionsWithReferenceId:(NSUInteger)referenceId referenceType:(PKReferenceType)referenceType {
+  NSString *uri = [NSString stringWithFormat:@"/subscription/%@/%ld", [PKConstants stringForReferenceType:referenceType], (unsigned long)referenceId];
+  PKRequest *request = [PKRequest requestWithURI:uri method:PKRequestMethodGET objectMapping:nil];
+
+  return request;
+}
+
+@end
